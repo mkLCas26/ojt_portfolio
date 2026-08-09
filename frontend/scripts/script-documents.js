f2f90b6b255ac391e47c57799f95d861fca9e08a
Kristina Cas <m.kristinacas@gmail.com>
2026-08-10 00:30:30 +0800

    feat(documents js): base code for documents upload in js

---
+++ frontend/scripts/script-documents.js
@@ -0,0 +1,75 @@
+// data for documents
+const documents = {
+    predeployment: {
+        title: "PRE-DEPLOYMENT",
+        
+        files: [
+            {
+                name: "Curriculum Vitae",
+                file: "../assets/documents/ojt-requirements.pdf"
+            }, 
+
+            {
+                name: "Approval Sheet",
+                file: "../assets/documents/ojt-requirements.pdf"
+            },
+
+            {
+                name: "Memorandum of Agreement",
+                file: "../assets/documents/ojt-requirements.pdf"
+            },
+
+            {
+                name: "Letter of Intent",
+                file: "../assets/documents/ojt-requirements.pdf"
+            },
+
+            {
+                name: "Letter of Endorsement",
+                file: "../assets/documents/ojt-requirements.pdf"
+            },
+
+            {
+                name: "Student Waiver",
+                file: "../assets/documents/ojt-requirements.pdf"
+            },
+
+            {
+                name: "Consent Form",
+                file: "../assets/documents/ojt-requirements.pdf"
+            },
+
+            {
+                name: "Medical Certificate",
+                file: "../assets/documents/ojt-requirements.pdf"
+            },
+
+            {
+                name: "Insurance",
+                file: "../assets/documents/ojt-requirements.pdf"
+            }
+        ]
+    },
+
+    evaluation: {
+        title: "EVALUATION",
+
+        files: [
+            {
+                name: "Certificate of Completion",
+                file: "../assets/documents/ojt-requirements.pdf"
+            },
+
+            {
+                name: "Performance of Evaluation Grading",
+                file: "../assets/documents/ojt-requirements.pdf"
+            },
+
+            {
+                name: "Evaluation for Supervisor",
+                file: "../assets/documents/ojt-requirements.pdf"
+            },
+        ]
+    }
+};
+
