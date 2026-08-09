31c4c0bc7872ed6bedc095bbe34af5ecad747f99
Kristina Cas <m.kristinacas@gmail.com>
2026-08-10 00:44:34 +0800

    feat(documents js): opening and closing folder action and functionalities

--- frontend/scripts/script-documents.js
+++ frontend/scripts/script-documents.js
@@ -100,3 +100,42 @@ const pdfViewer =
 
 const pdfFrame =
     document.getElementById("pdfFrame");
+
+// opening folder
+const folders = document.querySelectorAll(".folder_card");
+folders.forEach(folder => {
+    folder.addEventListener("click", () => {
+        const folderID = folder.dataset;
+        openFolder(folderID);
+    });
+});
+
+function openFolder(folderID) {
+    const folder = documents[folderID];
+    folderTitle.textContent = folder.title;
+
+    documentList.innerHTML = "";    // clear past docus
+
+    // for creating docu buttons
+    folder.files.forEach(file => {
+        const button = document.createElement("button");
+        button.classList.add("document_item");
+        button.textContent = file.name;
+
+        button.addEventListener("click", () => {
+            openDocument(file);
+        });
+
+        documentList.appendChild(button);
+    });
+
+    folderPanel.classList.add("active");
+};
+
+// closing folder
+document
+    .getElementById("closeFolder")
+    .addEventListener("click",  () => {
+        folderPanel.classList.remove("active");
+    });
+
